371b47aaf265c4a788b7b29e6071d9f51d02be86
AATaimah <129572284+AATaimah@users.noreply.github.com>
2024-02-09 23:12:00 -0500

    final product

--- Lab 4/sciprt.js
+++ Lab 4/sciprt.js
@@ -1,22 +1,20 @@
 
-  $(document).ready(function() {
+$(document).ready(function() {
     // Enable manual rotation with prev and next buttons
     $('.carousel').carousel({
-      interval: false // Disable automatic rotation
+        interval: false // Disable automatic rotation
     });
 
     // Enable automatic rotation after a delay
     setTimeout(function() {
-      $('.carousel').carousel({
-        interval: 5000 // Set interval to 5 seconds
-      });
+        $('.carousel').carousel({
+            interval: 5000 // Set interval to 5 seconds
+        });
     }, 2000); // Start automatic rotation after 2 seconds
 
     // Toggle additional details when "more info" link is clicked
-    $(document).ready(function(){
-      $(".more-info-link").click(function(event){
-          event.preventDefault(); // Prevent the default behavior of the link
-          $($(this).data("target")).toggle();
-      });
-  });
-  });
+    $(".more-info-link").click(function(event) {
+        event.preventDefault(); // Prevent the default behavior of the link
+        $($(this).data("target")).toggle();
+    });
+});
